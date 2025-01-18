cad21fd5ba4bd80031bfdabebd77bd7927429baa
siri2511 <siriraj123@hotmail.com>
2025-01-18 15:36:06 +0530

    Added Stack Implementation

---
+++ DSA/src/com/dsa/stackDemo/Stack.java
@@ -0,0 +1,53 @@
+package com.dsa.stackDemo;
+
+public class Stack {
+//	Stack(int n){
+//		this.size=n;
+//	}
+	int size=5;
+	int[] stack = new int[size];
+	int top=-1;
+	
+	//insert an element into the stack
+	public void push(int data) {
+		stack[++top]=data;
+	}
+	
+	//delete the last element of the stack
+	public int pop() {
+		int data;
+		data= stack[top];
+		stack[top]=0;
+		top--;
+		return data;
+	}
+	
+	//display the last element of the stack
+	public int peek() {
+		return stack[top];
+	}
+	
+	//display the stack elements
+	public void show() {
+		for(int i=0; i<=top; i++) {
+			System.out.print(stack[i]+" ");
+		}
+		System.out.println();
+	}
+	
+	//return the size of the stack
+	public int getSize() {
+		return top+1;
+	}
+	
+	//return if stack is empty or not
+	public boolean isEmpty() {
+		return top==-1;
+	}
+	
+	//return if stack is full or not
+	public boolean isFull() {
+		return top==size-1;
+	}
+	
+}
