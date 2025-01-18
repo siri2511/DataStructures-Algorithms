e8a25f6c67412e2bf7ecffb42882ad1b9e222636
siri2511 <siriraj123@hotmail.com>
2025-01-18 15:37:48 +0530

    Changes in stack package

---
+++ DSA/src/com/dsa/datastructures/stack/Test.java
@@ -0,0 +1,23 @@
+package com.dsa.datastructures.stack;
+
+//import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		//scan size of stack
+//		int n = sc.nextInt();
+//		sc.close();
+		Stack stack = new Stack();
+		stack.push(6);
+		stack.push(8);
+		stack.push(11);
+		System.out.println(stack.pop());
+		stack.push(10);
+		System.out.println(stack.peek());
+		stack.show();
+		System.out.println("Is the Stack Empty: "+stack.isEmpty());
+		System.out.println("Is the Stack Full: "+stack.isFull());
+		System.out.println("The size of the Stack: "+stack.getSize());
+	}
+}
